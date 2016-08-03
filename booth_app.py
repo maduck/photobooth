75cacd3276d0e84d47e8415cda645808315d8dc1
maduck <utti+github@uttisoft.de>
2016-08-03 16:32:18 +0000

    extracted color module

--- booth_app.py
+++ booth_app.py
@@ -8,15 +8,7 @@ from pygame_helpers import rounded_rect
 import RPi.GPIO as GPIO
 
 from config import Config
-
-
-class Colors(object):
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    RED = (255, 0, 0)
-
-    ORANGE = (200, 50, 20)
-    DARK_GRAY = (1, 1, 1)
+from colors import Colors
 
 
 class PhotoboothApp(object):
---
+++ colors.py
@@ -0,0 +1,9 @@
+
+
+class Colors(object):
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+
+    ORANGE = (200, 50, 20)
+    DARK_GRAY = (1, 1, 1)
