6726805ae996becfb19df982b13878de2dac65eb
maduck <utti+github@uttisoft.de>
2016-08-09 11:48:14 +0200

    Filled first camera interface: picamera. Still not using it.

--- backends/camera/picamera.py
+++ backends/camera/picamera.py
@@ -1,6 +1,29 @@
+import picamera
 from base_camera import BaseCameraBackend
 
 
 class CameraBackend(BaseCameraBackend):
-    pass
+
+    def __init__(self, config):
+        self.config = config
+        self.camera = picamera.PiCamera()
+        self.camera.annotate_text_size = 128
+        self.camera.led = False
+        self.camera.vflip = True
+        self.camera.resolution = (
+            self.config.getint("picture_width"),
+            self.config.getint("picture_height")
+        )
+
+    def start_preview(self):
+        self.camera.start_preview()
+
+    def stop_preview(self):
+        self.camera.stop_preview()
+
+    def take_photo(self, filename):
+        self.camera.capture(filename)
+
+    def cleanup(self):
+        pass
 
