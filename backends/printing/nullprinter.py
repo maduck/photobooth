649029da69c662dd1605e58a8ca982a352bdbc69
maduck <utti+github@uttisoft.de>
2016-08-11 07:20:35 +0200

    added a NullPrinter backend for not printing at all

---
+++ backends/printing/nullprinter.py
@@ -0,0 +1,9 @@
+from base_printing import BasePrintingBackend
+
+
+class PrintingBackend(BasePrintingBackend):
+    def __init__(self, config):
+        pass
+
+    def printout(self, filename):
+        return
