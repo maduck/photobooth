083615c3e7e455528721f57705005a1bb6a5781d
utti <utti+github@uttisoft.de>
2016-08-12 20:12:42 +0200

    removed useless string formatting values

--- backends/__init__.py
+++ backends/__init__.py
@@ -2,16 +2,14 @@ import importlib
 
 
 def get_class_name_for_backend(backend_type):
-    class_name = "{}{}".format(
+    class_name = "{}Backend".format(
         backend_type.capitalize(),
-        'Backend',
     )
     return class_name
 
 
 def acquire_backend(backend_type, backend_name, config):
-    import_class = "{}.{}.{}".format(
-        "backends",
+    import_class = "backends.{}.{}".format(
         backend_type,
         backend_name,
     )
