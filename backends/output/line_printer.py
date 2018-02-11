79d68e522571251a5b9eae48c276e0034c0e6901
MaDuck <maduck@users.noreply.github.com>
2018-02-11 20:16:14 +0100

    added some content to line printer backend

--- backends/output/line_printer.py
+++ backends/output/line_printer.py
@@ -1,3 +1,4 @@
+import tempfile
 import subprocess
 from base_saving import BaseOutputBackend
 
@@ -6,5 +7,8 @@ class OutputBackend(BaseOutputBackend):
     def __init__(self, config):
         self.config = config
 
-    def export(self, filename):
-        subprocess.call([self.config.get("command"), filename])
+    def export(self):
+        tmpfile = tempfile.NamedTemporaryFile()
+        # save file in here
+        subprocess.call([self.config.get("command"), tmpfile.name])
+        tmpfile.close()
